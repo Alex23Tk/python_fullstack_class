7e88cd8a933c4e31926f287a3b7069fb2d01dd91
Alex23Tk <122715363+Alex23Tk@users.noreply.github.com>
2024-07-15 17:47:15 +0700

    Module 2 Lesson 3

---
+++ task_2.py
@@ -0,0 +1,8 @@
+Worker_1: str = ("Света")
+Worker_2: str = ("Олег")
+Worker_3: str = ("Маша")
+Worker_4: str = ("Паша")
+
+print("В чётные дни работает:"+Worker_1+Worker_2)
+print()
+print("В нечётные дни работает:"+Worker_3+Worker_4)
