c3083636a666231f7ee23d5314d8f79dbbe28e14
Alex23Tk <122715363+Alex23Tk@users.noreply.github.com>
2024-07-16 14:48:12 +0700

    lesson_4

---
+++ lesson_4/task_1.py
@@ -0,0 +1,6 @@
+# Вввод числа с подсказкой
+first_num:float = input("Напишите число.")
+# Округление числа до целого
+round_fnum:float = round(first_num,0)
+# Вывод округлённого чилсла
+print("Результат:"+round_fnum)
